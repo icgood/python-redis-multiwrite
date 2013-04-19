5775badd39e83e420de0b0727872a95f84b0ee3f
Ian Good <ian.good@rackspace.com>
2013-04-19 13:50:46 -0400

    upped version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 from setuptools import setup
 
 setup(name='python-redis-multiwrite',
-      version='0.1.0',
+      version='0.1.1',
       description='Simultaneous redis write operations against many servers.',
       author='Ian Good',
       author_email='ian.good@rackspace.com',
